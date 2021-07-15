2f3e36a96b9acf37a070bd5f8f243183c1b0d8d0
Yurii <sheremet.yu@gmail.com>
2021-07-15 16:44:03 +0300

    initial commit

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -8,37 +8,35 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
-  form: FormGroup;
-  forbiddenUserNames = ['Anna', 'Yurii'];
+  signupForm: FormGroup;
+  forbiddenUsernames = ['Yurii', 'Anna'];
 
   ngOnInit() {
-    this.form = new FormGroup({
-      'userdata': new FormGroup({
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]) 
+        'email': new FormControl(null, [Validators.required, Validators.email]),  
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
-
-    // this.form.patchValue()
   }
 
   onSubmit() {
-    console.log(this.form);
+    console.log(this.signupForm);
   }
 
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
-    (<FormArray>this.form.get('hobbies')).push(control);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 
-  get controls() {
-    return (this.form.get('hobbies') as FormArray).controls;
+  getControls() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
   }
 
   forbiddenNames(control: FormControl): {[s: string]: boolean} {
-    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
       return {'nameIsForbidden': true};
     }
     return null;
--- http/src/app/app.module.ts
+++ http/src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    // FormsModule,
-    ReactiveFormsModule
+    BrowserModule, 
+    FormsModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
