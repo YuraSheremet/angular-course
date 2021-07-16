58642fdd269884abc351c977ff245f18116683da
Yurii <sheremet.yu@gmail.com>
2021-07-16 10:30:00 +0300

    http logic added

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -1,26 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Post } from './post.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  loadedPosts = [];
+export class AppComponent implements OnInit, OnDestroy {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+  error = null;
+  private sub: Subscription;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private postsService: PostsService
+  ) { }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.sub = this.postsService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    })
 
-  onCreatePost(postData: { title: string; content: string }) {
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts
+    }, error => {
+      this.isFetching = false;
+      this.error = error.message;
+    });
+  }
+
+  onCreatePost(postData: Post) {
     // Send Http request
+    // console.log(postData);
+    this.postsService.createStorePosts(postData.title, postData.content);
+    
   }
 
   onFetchPosts() {
     // Send Http request
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts
+    }, error => {
+      this.isFetching = false;
+      this.error = error.message;
+      console.log(error);
+    });
   }
 
   onClearPosts() {
     // Send Http request
+    this.postsService.deletePosts().subscribe(() => {
+      this.loadedPosts = [];
+    })
   }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
 }
---
+++ http/src/app/posts.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Subject, throwError } from "rxjs";
+import { map, catchError } from "rxjs/operators";
+import { Post } from "./post.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PostsService {
+    error = new Subject<string>();
+
+    constructor(private http: HttpClient) { }
+
+    createStorePosts(title: string, content: string) {
+        const postData: Post = { title: title, content: content };
+        this.http.post<{ name: string }>('https://http-88752-default-rtdb.firebaseio.com/posts.json', postData)
+            .subscribe(responseData => {
+                console.log(responseData);
+            }, error => {
+                this.error.next(error.message);
+            });
+    }
+
+    fetchPosts() {
+        return this.http.get<{ [key: string]: Post }>('https://http-88752-default-rtdb.firebaseio.com/posts.json')
+            .pipe(
+                map(responseData => {
+                    const postArray: Post[] = [];
+                    for (const key in responseData) {
+                        if (responseData.hasOwnProperty(key)) {
+                            postArray.push({ ...responseData[key], id: key })
+                        }
+                    }
+                    return postArray;
+                }),
+                catchError(errorRes => {
+                    return throwError(errorRes);
+                })
+            );
+    }
+
+    deletePosts() {
+        return this.http.delete('https://http-88752-default-rtdb.firebaseio.com/posts.json');
+    }
+}
